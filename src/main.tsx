d74ccc79f47e3db07a6b37b9fde8f4d27be3ad50
felipecarrillo100 <felipecarrillo100@gmail.com>
2025-09-23 20:37:52 +0200

    Implements 1) Coloring  on height and Intensity 2) On Label click the map zoom to the label.

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {StrictMode} from "react";
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import {StrictMode} from "react";
 
 // createRoot(document.getElementById('root')!).render(
-//         <App />
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
 // )
+
+createRoot(document.getElementById('root')!).render(
+        <App />
+)
